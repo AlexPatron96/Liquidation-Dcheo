3b1d4fb3a6c4d092a709ac702def354a31d81e1e
AlexPatron96 <114788024+AlexPatron96@users.noreply.github.com>
2023-04-16 23:24:04 -0500

    correcttion
    corrigiendo error en direccion de peticiones

--- src/utils/resources.jsx
+++ src/utils/resources.jsx
@@ -1,6 +1,6 @@
 export default {
 
-    URL_BASE: "http://31.220.53.218:8000/",
+    URL_BASE: "http://31.220.53.218:8000",
 
     URL_BASE2: "https://liquidationback.onrender.com/",
     // URL_BASE: "http://localhost:8000"
