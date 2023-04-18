7b7b9d21a488915b14cdf5e0ac497e9625bf0dcb
AlexPatron96 <114788024+AlexPatron96@users.noreply.github.com>
2023-04-18 13:34:27 -0500

    Update resources.jsx

--- src/utils/resources.jsx
+++ src/utils/resources.jsx
@@ -2,6 +2,6 @@ export default {
 
     //URL_BASE: "https://31.220.53.218:8000",
 
-    URL_BASE: "https://liquidation-appfront.onrender.com/",
+    URL_BASE: "https://liquidation-appfront.onrender.com",
     // URL_BASE: "http://localhost:8000"
 }
