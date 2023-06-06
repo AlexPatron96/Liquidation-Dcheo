714b941c717cec7c6c5dd01d7b24da4b955cf960
AlexPatron96 <alex.patron@utelvt.edu.ec>
2023-06-06 00:25:38 -0500

    ACtualization The Search and download.

--- src/routes/dashboard/Invoice.jsx
+++ src/routes/dashboard/Invoice.jsx
@@ -1,191 +1,299 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import Buttonatom from '../../components/atom/Buttonatom';
-import Functionalitiesbtn from '../../components/atom/Functionalitiesbtn';
-import Createdcustomer from '../../components/creators/Createdcustomer';
-import Tabledinamik from '../../components/molecules/Tabledinamik';
-import Paginationdesign from '../../components/Paginationdesign';
-import LoadingScreen from '../../layout/LoadingScreen';
-import { deleteInvoiceThunk, getInvoiceThunk, postInvoiceTransacthunk, postInvoicethunk, updateInvoiceThunk } from '../../store/slices/invoice.slice';
-import { setPagination } from '../../store/slices/pagination.slice';
-import Swal from 'sweetalert2';
-import TableInvoice from '../../components/Show/TableInvoice';
-import { getRouteDayThunk } from '../../store/slices/routeday.slice';
-import { setErrorReceived } from '../../store/slices/errorReceived.slice';
-import ModalTransaccion from '../../components/Modals/ModalTransaccion';
-import ModalInvoiceTransac from '../../components/Modals/ModalInvoiceTransac';
-import { getSellerThunk } from '../../store/slices/seller.slice';
-import { getCustomerThunk } from '../../store/slices/customer.slice';
-import Formselectatom from '../../components/atom/Formselectatom';
-import Filterinvoice from '../../components/Modals/Filterinvoice';
-
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Buttonatom from "../../components/atom/Buttonatom";
+import Functionalitiesbtn from "../../components/atom/Functionalitiesbtn";
+import Createdcustomer from "../../components/creators/Createdcustomer";
+import Tabledinamik from "../../components/molecules/Tabledinamik";
+import Paginationdesign from "../../components/Paginationdesign";
+import LoadingScreen from "../../layout/LoadingScreen";
+import {
+	deleteInvoiceThunk,
+	getInvoiceThunk,
+	postInvoiceTransacthunk,
+	postInvoicethunk,
+	updateInvoiceThunk,
+} from "../../store/slices/invoice.slice";
+import { setPagination } from "../../store/slices/pagination.slice";
+import Swal from "sweetalert2";
+import TableInvoice from "../../components/Show/TableInvoice";
+import { getRouteDayThunk } from "../../store/slices/routeday.slice";
+import { setErrorReceived } from "../../store/slices/errorReceived.slice";
+import ModalTransaccion from "../../components/Modals/ModalTransaccion";
+import ModalInvoiceTransac from "../../components/Modals/ModalInvoiceTransac";
+import { getSellerThunk } from "../../store/slices/seller.slice";
+import { getCustomerThunk } from "../../store/slices/customer.slice";
+import Formselectatom from "../../components/atom/Formselectatom";
+import Filterinvoice from "../../components/Modals/Filterinvoice";
+// import { writeFile, utils } from "xlsx";
+import { saveAs } from "file-saver";
+import ExcelJS from "exceljs";
+import date from "../../utils/date";
+import { setIsLoading } from "../../store/slices/isLoading.slice";
 
 const Invoice = () => {
+	const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
-    // /************ VERIFICA SI NO HAY UN ERROR EN EL SLICE DE ERROR ****************/
-    // const errorReceived = useSelector(state => state.errorReceived);
-    // errorReceived.length === 0 ? null : Swal.fire({
-    //     title: "Error",
-    //     text: `Existe un error en esta operacion : ${errorReceived.error} `,
-    //     icon: 'error',
-    //     confirmButtonColor: '#d33',
-    //     confirmButtonText: 'OK'
-    // }).then((result) => {
-    //     if (result.isConfirmed) {
-    //         dispatch(setErrorReceived([]))
-    //     }
-    // });
-    // /**************************************************************/
-    useEffect(() => {
-        invoice[0] ? null : dispatch(getInvoiceThunk());
-        routeDay[0] ? null : dispatch(getRouteDayThunk());
-    }, [])
+	useEffect(() => {
+		invoice[0] ? null : dispatch(getInvoiceThunk());
+		routeDay[0] ? null : dispatch(getRouteDayThunk());
+	}, []);
 
-    const routeDay = useSelector(state => state.routeDay);
-    const invoice = useSelector(state => state.invoice);
-    const loading = useSelector(state => state.isLoading);
-    const pagination = useSelector(state => state.pagination);
+	const routeDay = useSelector((state) => state.routeDay);
+	const invoice = useSelector((state) => state.invoice);
+	const loading = useSelector((state) => state.isLoading);
+	const pagination = useSelector((state) => state.pagination);
+	const user = useSelector((state) => state.userLoged);
+	console.log(invoice);
 
-    // console.log(invoice);
-    const [modalShow, setModalShow] = useState(false);
+	// console.log(invoice);
+	const [modalShow, setModalShow] = useState(false);
 
-    const createdCustomer = () => {
-        if (!modalShow) {
-            setModalShow(true)
-        } else {
-            setModalShow(false)
-        }
-    }
+	const createdCustomer = () => {
+		if (!modalShow) {
+			setModalShow(true);
+		} else {
+			setModalShow(false);
+		}
+	};
+	const dataIfn = [
+		{ name: "John Doe", age: 25, email: "john@example.com" },
+		{ name: "Jane Smith", age: 30, email: "jane@example.com" },
+		// Agrega más datos aquí...
+	];
+	const btnCreated = () => {
+		return (
+			<>
+				<Buttonatom
+					created={createdCustomer}
+					title={"Crear Cliente"}
+					color={"success"}
+					ico={"fa-circle-plus"}
+				/>
+				<Buttonatom
+					created={transaccionPay}
+					title={"Pago-Abono"}
+					color={"success"}
+					ico={"fa-sack-dollar"}
+				/>
+				<Buttonatom
+					created={refresh}
+					title={""}
+					color={"info"}
+					ico={"fa-arrow-rotate-right bx-spin-hover"}
+				/>
+				<Buttonatom
+					created={() => setModalFilter(true)}
+					title={""}
+					color={"primary"}
+					ico={"fa-filter"}
+				/>
 
-    const btnCreated = () => {
-        return (
-            <>
-                <Buttonatom created={createdCustomer}
-                    title={"Crear Cliente"}
-                    color={"success"} ico={"fa-circle-plus"} />
-                <Buttonatom created={transaccionPay}
-                    title={"Pago o Abono"}
-                    color={"success"} ico={"fa-sack-dollar"} />
-                <Buttonatom created={refresh}
-                    title={""}
-                    color={"info"} ico={"fa-arrow-rotate-right bx-spin-hover"} />
-                <Buttonatom created={() => setModalFilter(true)}
-                    title={""}
-                    color={"primary"} ico={"fa-filter"} />
-            </>
-        )
-    }
-    /************************************************************************************** */
+				<Buttonatom
+					created={() => generarArchivoExcel(pagination)}
+					title={""}
+					color={"warning"}
+					ico={"fa-download"}
+				/>
+			</>
+		);
+	};
+	/************************************************************************************** */
 
-    const createInvo = (data) => {
-        Swal.fire({
-            position: 'center',
-            icon: 'success',
-            title: "Se a creado una Factura nueva",
-            showConfirmButton: false,
-            timer: 1500
-        })
-        dispatch(postInvoiceTransacthunk(data));
-    };
+	const createInvo = (data) => {
+		Swal.fire({
+			position: "center",
+			icon: "success",
+			title: "Se a creado una Factura nueva",
+			showConfirmButton: false,
+			timer: 1500,
+		});
+		dispatch(postInvoiceTransacthunk(data));
+	};
 
-    const updateInvo = (id, data) => {
-        dispatch(updateInvoiceThunk(id, data));
-    };
+	const updateInvo = (id, data) => {
+		dispatch(updateInvoiceThunk(id, data));
+	};
 
-    const delInvo = (id) => {
-        dispatch(deleteInvoiceThunk(id));
-    };
+	const delInvo = (id) => {
+		dispatch(deleteInvoiceThunk(id));
+	};
 
-    const search = (data) => {
+	const search = (data) => {
+		const filteredList = invoice.filter(
+			(item) =>
+				(item?.client?.fullname)
+					.toLowerCase()
+					.includes(data.toLowerCase()) ||
+				(item?.client?.dni).includes(data) ||
+				(item?.num_bill).includes(data) ||
+				(item?.deliver_date).includes(data) ||
+				(item?.seller.code).toLowerCase().includes(data) ||
+				(item?.seller.name).toLowerCase().includes(data)
+		);
+		dispatch(setPagination(filteredList));
+		// console.log(filteredList);
+	};
 
-        const filteredList = invoice.filter((item) =>
-        (
-            (item?.client?.fullname).toLowerCase().includes((data).toLowerCase()) ||
-            (item?.client?.dni).includes(data) ||
-            (item?.num_bill).includes(data) ||
-            (item?.deliver_date).includes(data) ||
-            (item?.seller.code).toLowerCase().includes(data) ||
-            (item?.seller.name).toLowerCase().includes(data)
-        ));
-        dispatch(setPagination(filteredList));
-        // console.log(filteredList);
-    };
+	const [modalTransaccionPay, setModalTransaccionPay] = useState(false);
+	const [itemSelect, setItemSelect] = useState("");
 
-    const [modalTransaccionPay, setModalTransaccionPay] = useState(false);
-    const [itemSelect, setItemSelect] = useState("");
+	const transaccionPay = (item) => {
+		setModalTransaccionPay(true);
+		setItemSelect(item);
+	};
 
-    const transaccionPay = (item) => {
-        setModalTransaccionPay(true);
-        setItemSelect(item);
-    };
+	const refresh = () => {
+		dispatch(getInvoiceThunk());
+		dispatch(getRouteDayThunk());
+		dispatch(getSellerThunk());
+		dispatch(getCustomerThunk());
+	};
 
-    const refresh = () => {
-        dispatch(getInvoiceThunk());
-        dispatch(getRouteDayThunk());
-        dispatch(getSellerThunk());
-        dispatch(getCustomerThunk());
-    };
+	const selecionSearch = (e) => {
+		const { name, value } = e.target;
+		console.log(value);
+	};
+	const searchDB = [
+		{ id: 1, detail: "LOCAL" },
+		{ id: 2, detail: "BASE DATOS" },
+	];
+	const listAvailable = () => {
+		return (
+			<>
+				<Formselectatom
+					title={"Selecionar lugar de busqueda"}
+					iterador={searchDB}
+					dataSelect={selecionSearch}
+					firstdata={"detail"}
+					secunddata={"dia"}
+					disabledAction={false}
+				/>
+			</>
+		);
+	};
 
-    const selecionSearch = (e) => {
-        const { name, value } = e.target;
-        console.log(value);
-    };
-    const searchDB = [
-        { "id": 1, "detail": "LOCAL" },
-        { "id": 2, "detail": "BASE DATOS" },
-    ];
-    const listAvailable = () => {
-        return (
-            <>
-                <Formselectatom title={"Selecionar lugar de busqueda"}
-                    iterador={searchDB}
-                    dataSelect={selecionSearch}
-                    firstdata={"detail"}
-                    secunddata={"dia"}
-                    disabledAction={false} />
-            </>
-        )
-    };
+	const [modalFilter, setModalFilter] = useState(false);
+	const onHideFilter = () => {
+		setModalFilter(false);
+	};
 
+	const generarArchivoExcel = (data) => {
+		// Crear un nuevo libro de Excel
+		dispatch(setIsLoading(true));
+		const workbook = new ExcelJS.Workbook();
+		const worksheet = workbook.addWorksheet("Datos");
 
-    const [modalFilter, setModalFilter] = useState(false);
-    const onHideFilter = () => {
-        setModalFilter(false);
-    };
+		const sumBalance = data.reduce(
+			(total, obj) => total + parseFloat(obj.balance),
+			0
+		);
+		const sumTotal = data.reduce(
+			(total, obj) => total + parseFloat(obj.total_bill),
+			0
+		);
 
-    return (
-        <div className='pages'>
-            <h2>Facturas</h2>
-            <Functionalitiesbtn
-                buttons={btnCreated}
-                // listAvailable={listAvailable}
-                search={search} />
+		worksheet.addRow([" ", " ", `Distribuidora de CHEO`]);
+		worksheet.addRow([
+			" ",
+			`Generado Por: ${user.fullname}`,
+			`Fecha: ${date.CurrendateDay()} ${date.Currendate()}`,
+			`Tipo: ${user.username}`,
+		]);
+		worksheet.addRow([" ", " "]);
+		worksheet.addRow([
+			"ID",
+			"Cliente",
+			"# Documento",
+			"Tipo DOCUMENTO",
+			"Estatus",
+			"Fecha Entrega",
+			"Total",
+			"Saldo Actual",
+			"Detalle",
+			"Vendedor",
+		]);
+		// Escribir los datos en la hoja de trabajo
+		data.forEach((datum) => {
+			worksheet.addRow([
+				datum.id,
+				datum.client.fullname,
+				datum.num_bill,
+				datum.isWhite ? "Nota de Venta" : "Factura",
+				datum.id_status === "1"
+					? "Pendiente"
+					: datum.id_status === "2"
+					? "Abonada"
+					: "Pagada",
+				`(${date.DatePastPresent(datum.deliver_date)})-${
+					datum.deliver_date
+				}`,
+				`${parseFloat(datum.total_bill).toFixed(2)}`,
+				`${parseFloat(datum.balance).toFixed(2)}`,
+				datum.detail,
+				datum.seller.name,
+			]);
+		});
 
-            <TableInvoice
-                data={pagination}
-                updateInvo={updateInvo}
-                delInvo={delInvo}
-                transaccionPay={transaccionPay}
-                createInvo={createInvo} />
+		worksheet.addRow([
+			"",
+			"",
+			"",
+			"",
+			"",
+			"Totales:",
+			`$ ${parseFloat(sumTotal).toFixed(2)}`,
+			`$ ${parseFloat(sumBalance).toFixed(2)}`,
+		]);
+		// Generar el archivo Excel
 
-            {!loading ? <Paginationdesign
-                data={"invoice"}
-            />
-                : <LoadingScreen />
-            }
-            <Createdcustomer
-                show={modalShow}
-                onHide={() => setModalShow(false)}
-                title={"Created Customer"}
-            />
-            {/* <ModalInvoiceTransac onHide={() => { setModalTransaccionPay(false) }} show={modalTransaccionPay} /> */}
-            <ModalInvoiceTransac itemSelect={itemSelect} onhide={() => setModalTransaccionPay(false)} show={modalTransaccionPay} />
+		workbook.xlsx
+			.writeBuffer()
+			.then((buffer) => {
+				const blob = new Blob([buffer], {
+					type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+				});
+				saveAs(blob, "datos.xlsx");
+			})
+			.finally(dispatch(setIsLoading(false)));
+	};
 
-            <Filterinvoice itemSelect={itemSelect} onhide={() => setModalFilter(false)} show={modalFilter} />
+	return (
+		<div className="pages">
+			<h2>Facturas</h2>
+			<Functionalitiesbtn
+				buttons={btnCreated}
+				// listAvailable={listAvailable}
+				search={search}
+			/>
 
-        </div>
-    );
+			<TableInvoice
+				data={pagination}
+				updateInvo={updateInvo}
+				delInvo={delInvo}
+				transaccionPay={transaccionPay}
+				createInvo={createInvo}
+			/>
+
+			{!loading ? <Paginationdesign data={"invoice"} /> : <LoadingScreen />}
+			<Createdcustomer
+				show={modalShow}
+				onHide={() => setModalShow(false)}
+				title={"Created Customer"}
+			/>
+			{/* <ModalInvoiceTransac onHide={() => { setModalTransaccionPay(false) }} show={modalTransaccionPay} /> */}
+			<ModalInvoiceTransac
+				itemSelect={itemSelect}
+				onhide={() => setModalTransaccionPay(false)}
+				show={modalTransaccionPay}
+			/>
+
+			<Filterinvoice
+				itemSelect={itemSelect}
+				onhide={() => setModalFilter(false)}
+				show={modalFilter}
+			/>
+		</div>
+	);
 };
 
 export default Invoice;
--- src/routes/dashboard/Sellers.jsx
+++ src/routes/dashboard/Sellers.jsx
@@ -1,159 +1,163 @@
-import React, { useEffect, useState } from 'react';
-import { getRoutethunk } from '../../store/slices/dataTemp.slice';
-import { useDispatch, useSelector } from 'react-redux';
-import { deleteSellerThunk, getSellerThunk, updateSellerThunk } from '../../store/slices/seller.slice';
-import Buttonatom from '../../components/atom/Buttonatom';
-import TableList from '../../components/TableList';
-import LoadingScreen from '../../layout/LoadingScreen';
-import Paginationdesign from '../../components/Paginationdesign'
-import Functionalitiesbtn from '../../components/atom/Functionalitiesbtn';
-import Createdseller from '../../components/molecules/Createdseller';
-import Formselectatom from '../../components/atom/Formselectatom';
+import React, { useEffect, useState } from "react";
+import { getRoutethunk } from "../../store/slices/dataTemp.slice";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  deleteSellerThunk,
+  getSellerThunk,
+  updateSellerThunk,
+} from "../../store/slices/seller.slice";
+import Buttonatom from "../../components/atom/Buttonatom";
+import TableList from "../../components/TableList";
+import LoadingScreen from "../../layout/LoadingScreen";
+import Paginationdesign from "../../components/Paginationdesign";
+import Functionalitiesbtn from "../../components/atom/Functionalitiesbtn";
+import Createdseller from "../../components/molecules/Createdseller";
+import Formselectatom from "../../components/atom/Formselectatom";
 import verify from "../../img/verificado.gif";
-import { setPagination } from '../../store/slices/pagination.slice';
-import CreateSellersClouster from '../../components/creators/CreateSellersClouster';
-import CreateSeller from '../../components/creators/CreateSeller';
-import TableSellers from '../../components/Show/TableSellers';
-import { setErrorReceived } from '../../store/slices/errorReceived.slice';
-import Swal from 'sweetalert2';
-
+import { setPagination } from "../../store/slices/pagination.slice";
+import CreateSellersClouster from "../../components/creators/CreateSellersClouster";
+import CreateSeller from "../../components/creators/CreateSeller";
+import TableSellers from "../../components/Show/TableSellers";
+import { setErrorReceived } from "../../store/slices/errorReceived.slice";
+import Swal from "sweetalert2";
 
 const Sellers = () => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  // /************ VERIFICA SI NO HAY UN ERROR EN EL SLICE DE ERROR ****************/
+  // const errorReceived = useSelector(state => state.errorReceived);
+  // errorReceived.length === 0 ? null : Swal.fire({
+  //     title: "Error",
+  //     text: `Existe un error en esta operacion : ${errorReceived.error} `,
+  //     icon: 'error',
+  //     confirmButtonColor: '#d33',
+  //     confirmButtonText: 'OK'
+  // }).then((result) => {
+  //     if (result.isConfirmed) {
+  //         dispatch(setErrorReceived([]))
+  //     }
+  // });
+  // /**************************************************************/
 
-    // /************ VERIFICA SI NO HAY UN ERROR EN EL SLICE DE ERROR ****************/
-    // const errorReceived = useSelector(state => state.errorReceived);
-    // errorReceived.length === 0 ? null : Swal.fire({
-    //     title: "Error",
-    //     text: `Existe un error en esta operacion : ${errorReceived.error} `,
-    //     icon: 'error',
-    //     confirmButtonColor: '#d33',
-    //     confirmButtonText: 'OK'
-    // }).then((result) => {
-    //     if (result.isConfirmed) {
-    //         dispatch(setErrorReceived([]))
-    //     }
-    // });
-    // /**************************************************************/
+  useEffect(() => {
+    sellerRedux[0] ? null : dispatch(getSellerThunk());
+  }, []);
 
-    useEffect(() => {
-        sellerRedux[0] ? null : dispatch(getSellerThunk());
-    }, [])
+  const sellerRedux = useSelector((state) => state.seller);
+  // const route = useSelector(state => state.temporary);
+  const loading = useSelector((state) => state.isLoading);
+  const pagination = useSelector((state) => state.pagination);
 
-    const sellerRedux = useSelector(state => state.seller);
-    // const route = useSelector(state => state.temporary);
-    const loading = useSelector(state => state.isLoading);
-    const pagination = useSelector(state => state.pagination);
+  const listShow = ["#id", "Nombre", "Activo", "Ruta"];
+  const listDB = ["nombre", "isActive", "id_route"];
 
-    const listShow = ["#id", "Nombre", "Activo", "Ruta"];
-    const listDB = ["nombre", "isActive", "id_route"];
+  console.log(sellerRedux);
 
+  const [modalShow, setModalShow] = useState(false);
 
+  const updateData = (id, data) => {
+    dispatch(updateSellerThunk(id, data));
+  };
+  const deleteData = (id) => {
+    // console.log(id);
+    dispatch(deleteSellerThunk(id));
+  };
 
-    const [modalShow, setModalShow] = useState(false);
-
-    const updateData = (id, data) => {
-        dispatch(updateSellerThunk(id, data))
+  const createdSeller = () => {
+    if (!modalShow) {
+      setModalShow(true);
+      // console.log("mostrar model Vendedor");
+    } else {
+      setModalShow(false);
     }
-    const deleteData = (id) => {
-       // console.log(id);
-        dispatch(deleteSellerThunk(id));
-    }
+  };
+  const [showCreateByClouster, setShowCreateByClouster] = useState(false);
 
-    const createdSeller = () => {
-        if (!modalShow) {
-            setModalShow(true)
-           // console.log("mostrar model Vendedor");
-        } else {
-            setModalShow(false)
-        }
+  const createByClouster = () => {
+    if (!showCreateByClouster) {
+      setShowCreateByClouster(true);
+      // console.log("mostrar model created showCreateByClouster");
+    } else {
+      setShowCreateByClouster(false);
     }
-    const [showCreateByClouster, setShowCreateByClouster] = useState(false);
+  };
 
-    const createByClouster = () => {
-        if (!showCreateByClouster) {
-            setShowCreateByClouster(true)
-           // console.log("mostrar model created showCreateByClouster");
-        } else {
-            setShowCreateByClouster(false)
-        }
-    }
-
-    const btnCreated = () => {
-        return (
-            <>
-                <Buttonatom created={createdSeller}
-                    title={"Crear Vendedor"}
-                    color={"success"} ico={"fa-circle-plus"} />
-                <Buttonatom created={createByClouster}
-                    title={""}
-                    color={"success"} ico={"fa-cloud-arrow-up"} />
-                <Buttonatom created={refresh}
-                    title={""}
-                    color={"info"} ico={"fa-arrow-rotate-right bx-spin-hover"} />
-            </>
-        )
-    }
-    const listAvailable = () => {
-        return (
-            <>
-                {/* <Formselectatom title={"Ver Rutas Disponibles"}
+  const btnCreated = () => {
+    return (
+      <>
+        <Buttonatom
+          created={createdSeller}
+          title={"Crear Vendedor"}
+          color={"success"}
+          ico={"fa-circle-plus"}
+        />
+        <Buttonatom
+          created={createByClouster}
+          title={""}
+          color={"success"}
+          ico={"fa-cloud-arrow-up"}
+        />
+        <Buttonatom
+          created={refresh}
+          title={""}
+          color={"info"}
+          ico={"fa-arrow-rotate-right bx-spin-hover"}
+        />
+      </>
+    );
+  };
+  const listAvailable = () => {
+    return (
+      <>
+        {/* <Formselectatom title={"Ver Rutas Disponibles"}
                     iterador={route}
                     firstdata={"id"}
                     secunddata={"dia"}
                     disabledAction={true} /> */}
-            </>
-        )
-    }
-
-    const search = (data) => {
-        const filteredList = sellerRedux.filter((item) =>
-        (
-            (item.name)?.toLowerCase().includes(data.toLowerCase()) ||
-            (item.code)?.toLowerCase().includes(data.toLowerCase()) ||
-            ((item.isActive) === true ? "si" : "no") === data.toLowerCase()
-        ));
-        dispatch(setPagination(filteredList));
-    }
-
-    const refresh = () => {
-        dispatch(getSellerThunk());
-    };
-
-    return (
-        <div className='sellers pages'>
-            <div>
-                <h2>
-                    Vendedores
-                </h2>
-                <Functionalitiesbtn
-                    buttons={btnCreated}
-                    listAvailable={listAvailable}
-                    search={search} />
-            </div>
-            <div>
-                <TableSellers
-                    data={pagination}
-                    updateData={updateData}
-                    deleteData={deleteData}
-                />
-                {!loading ? <Paginationdesign
-                    data={'seller'}
-                />
-                    : <LoadingScreen />
-                }
-            </div>
-            <CreateSeller show={modalShow}
-                onHide={() => setModalShow(false)}
-            />
-
-            <CreateSellersClouster
-                show={showCreateByClouster}
-                onHide={() => setShowCreateByClouster(false)} />
-
-        </div>
+      </>
     );
+  };
+
+  const search = (data) => {
+    const filteredList = sellerRedux.filter(
+      (item) =>
+        item.name?.toLowerCase().includes(data.toLowerCase()) ||
+        item.code?.toLowerCase().includes(data.toLowerCase()) ||
+        (item.isActive === true ? "si" : "no") === data.toLowerCase()
+    );
+    dispatch(setPagination(filteredList));
+  };
+
+  const refresh = () => {
+    dispatch(getSellerThunk());
+  };
+
+  return (
+    <div className="sellers pages">
+      <div>
+        <h2>Vendedores</h2>
+        <Functionalitiesbtn
+          buttons={btnCreated}
+          listAvailable={listAvailable}
+          search={search}
+        />
+      </div>
+      <div>
+        <TableSellers
+          data={pagination}
+          updateData={updateData}
+          deleteData={deleteData}
+        />
+        {!loading ? <Paginationdesign data={"seller"} /> : <LoadingScreen />}
+      </div>
+      <CreateSeller show={modalShow} onHide={() => setModalShow(false)} />
+
+      <CreateSellersClouster
+        show={showCreateByClouster}
+        onHide={() => setShowCreateByClouster(false)}
+      />
+    </div>
+  );
 };
 
 export default Sellers;
